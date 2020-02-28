128b34df83b1fa0b9c240211836e4f2f338c2b3f
tkong233 <tkong@wesleyan.edu>
2020-02-27 23:45:29 -0500

    AllSubsets

---
+++ src/DFS/AllSubsets.java
@@ -0,0 +1,24 @@
+package DFS;
+
+public class AllSubsets {
+    void allSubsets(char[] input) {
+        allSubsetsHelper(input, 0, new StringBuilder());
+    }
+    void allSubsetsHelper(char[] input, int index, StringBuilder solutionPrefix) {
+        if (index == input.length) {
+            System.out.println(solutionPrefix);
+            return;
+        }
+
+        solutionPrefix.append(input[index]);
+        allSubsetsHelper(input, index + 1, solutionPrefix);
+        solutionPrefix.deleteCharAt(solutionPrefix.length() - 1);
+        allSubsetsHelper(input, index + 1, solutionPrefix);
+    }
+
+    public static void main(String[] args) {
+        AllSubsets solution = new AllSubsets();
+        solution.allSubsets(new char[] {'a', 'b', 'c'});
+    }
+
+}
